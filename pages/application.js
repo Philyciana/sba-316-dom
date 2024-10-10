e92b53b77e502cbc46ec95ddc9023f80f6950018
Philyciana <179555132+Philyciana@users.noreply.github.com>
2024-10-10 02:48:04 -0400

    event listener for text content and using sibling relationship to navigate where to put a file input

--- pages/application.js
+++ pages/application.js
@@ -1,6 +1,7 @@
 const infoForm = document.getElementById('basic-info');
 const labelName = document.createElement('label');
-infoForm.appendChild(labelName);
+const dobInput = document.getElementById('date')
+infoForm.insertBefore(labelName, dobInput)
 labelName.textContent = "Date of Birth:";
 
 const allLabels = document.querySelectorAll('label');
@@ -8,3 +9,16 @@ const allLabels = document.querySelectorAll('label');
 for ( let i = 0; i < allLabels.length; i++) {
     allLabels[i].style.fontSize = "20px";
 }
+
+const jobForm = document.getElementById('job-info');
+const cvLabel = document.getElementById('coverletter')
+const resumeInput = document.createElement('input');
+resumeInput.setAttribute('type', 'file');
+resumeInput.setAttribute('id', 'userResume');
+const resumeLabel = document.getElementById('resumelabel')
+jobForm.insertBefore(resumeInput, resumeLabel.nextSibling);
+
+resumeInput.addEventListener("change", function(){
+    if (resumeInput.files.length > 0 ) {
+        resumeLabel.textContent = "A file has been uploaded as your resume!"
+        resumeLabel.style.color = 'red';}});
