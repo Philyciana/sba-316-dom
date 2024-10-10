d66a974f4f7ca0f5aeaf5044f33eaf707cd6ff90
Philyciana <179555132+Philyciana@users.noreply.github.com>
2024-10-10 15:52:29 -0400

    Adding doc frag to thank user when application is submitted

--- pages/application.js
+++ pages/application.js
@@ -13,12 +13,42 @@ for ( let i = 0; i < allLabels.length; i++) {
 const jobForm = document.getElementById('job-info');
 const cvLabel = document.getElementById('coverletter')
 const resumeInput = document.createElement('input');
+const cvInput = document.createElement('input');
+cvInput.setAttribute('type', 'file');
+cvInput.setAttribute('id', 'userCV');
 resumeInput.setAttribute('type', 'file');
 resumeInput.setAttribute('id', 'userResume');
-const resumeLabel = document.getElementById('resumelabel')
+const resumeLabel = document.getElementById('resumelabel');
+jobForm.insertBefore(cvInput, cvLabel.nextSibling)
 jobForm.insertBefore(resumeInput, resumeLabel.nextSibling);
 
-resumeInput.addEventListener("change", function(){
+function resumeFileAdded(){
     if (resumeInput.files.length > 0 ) {
         resumeLabel.textContent = "A file has been uploaded as your resume!"
-        resumeLabel.style.color = 'red';}});
+        resumeLabel.style.color = 'red';}}
+
+resumeInput.addEventListener("change", resumeFileAdded);
+
+function cvFileAdded(){
+    if (cvInput.files.length > 0 ) {
+        cvLabel.textContent = "A file has been uploaded as your cover letter!"
+        cvLabel.style.color = 'green';}};
+cvInput.addEventListener("change", cvFileAdded);
+
+const newLine = document.createElement('br');
+jobForm.insertBefore(newLine, cvInput.nextSibling);
+
+const appSubmitButton = document.getElementById('submit-button')
+
+function thankApplicant (){
+    const frag = document.createDocumentFragment();
+    const thankingHeader = frag.appendChild(document.createElement("h3"));
+    thankingHeader.textContent = "Thank you for applying to Target!";
+    frag.appendChild(thankingHeader);
+    
+    const thankingpara = frag.appendChild(document.createElement("p"));
+    thankingpara.textContent = "We will get back to you about your application shortly";
+    frag.appendChild(thankingpara);
+    document.body.appendChild(frag);
+}
+appSubmitButton.addEventListener("click", thankApplicant);
